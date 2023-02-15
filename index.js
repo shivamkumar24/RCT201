bad990e94436c1ca46bf8de9702f600b8b9b3e26
Shivam <shivam2432000@gmail.com>
2023-02-16 00:36:18 +0530

    This is file added

--- index.js
+++
@@ -1,9 +0,0 @@
-function Person(first, last, age) {
-  this.firstName = first;
-  this.lastName = last;
-  this.age = age;
-}
-
-const details = new Person("Shivam", "Kumar", 22);
-Person.prototype.nationality = "Indian";
-console.log(details.nationality);
